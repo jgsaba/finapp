0b4a9c12699b69bcb378eed87acc89bfd0059e0c
pve_jjorge <pve_jjorge@uolinc.com>
2022-05-02 20:57:23 -0300

    first

---
+++ src/main/java/ir/com/betterbankingio/finapp/FinappApplication.java
@@ -0,0 +1,13 @@
+package ir.com.betterbankingio.finapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinappApplication.class, args);
+    }
+
+}
